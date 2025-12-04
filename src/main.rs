1b77b2d250094bb81bdffd727f2a5fabe1ae220f
User <nevo.krien@gmail.com>
2025-12-04 22:54:04 +0200

    part2

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,39 @@ fn main() {
     let mut count = 0;
     let mut dial = 50;
     for l in input.lines(){
+        println!("in {l} with {dial}");
+
         let (dir,rest) = l.split_at(1);
         let amount : u32 = rest.parse().unwrap();
+        
+        let full_spins = amount/100;
+        if full_spins>0{
+            println!("adding {} because of full spins",amount/100);
+        }
+
+        count += full_spins;
         let amount = amount%100;
 
-        dial=match dir {
-            "L"=>(dial+100-amount)%100,
-            "R"=>(dial+amount)%100,
+        match dir {
+            "L"=>{
+                if amount>=dial && dial!=0{
+                    println!("looped neg");
+                    count+=1;
+                }
+                dial=(dial+100-amount)%100;
+
+            },
+            "R"=>{
+                if amount>=100-dial && dial!=0{
+                    println!("looped pos");
+                    count+=1;
+                }
+                dial=(dial+amount)%100;
+            },
             r=>panic!("unexpected dir {r}"),
         };
 
-        if dial==0{
-            count+=1;
-        }
+
     }
 
     println!("count is {count}", );
